9771bad0ff414e220251f69725025ac2ea5fc36d
Travis Horesh <horeshtr@gmail.com>
2022-12-09 08:47:18 -0500

    Code takes inputs for state and date range. Still working on outputs

--- datacamp_alien_sightings.R
+++ datacamp_alien_sightings.R
@@ -10,6 +10,7 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(lubridate)
 
 # Read in complete dataset
 data <- read.csv("https://query.data.world/s/pgcgsk36intv4dgunw6k5y73tl7cjc", header=TRUE, stringsAsFactors=FALSE);
@@ -17,7 +18,11 @@ glimpse(data)
 head(data)
 
 # Clean and transform
-
+data <- data %>% 
+  mutate(
+    date_time = as.POSIXct(date_time, tz = "UTC", "%Y-%m-%dT%H:%M:%S"), 
+    posted = as.POSIXct(posted, tz = "UTC", "%Y-%m-%dT%H:%M:%S"))
+glimpse(data)
 
 # Subset to only USA data
 data_usa <- data %>% filter(country == "USA")
@@ -30,24 +35,31 @@ ui <- fluidPage(
     # Application title
     titlePanel("USA UFO Sightings Data"),
 
-    # Sidebar with a slider input for number of bins 
+    # Sidebar with dropdown and date selectors 
     sidebarLayout(
         sidebarPanel(
           # CODE BELOW: One input to select a U.S. state
           # And one input to select a range of dates
           selectInput('state', 'Select a State:', choices = unique(data_usa$state)),
           dateRangeInput('dates', 'Select a Date Range:', 
-                         min = min(data_usa$date_time),
-                         max = max(data_usa$date_time))
+                         start = min(data_usa$date_time),
+                         end = max(data_usa$date_time))
           ),
 
-        # Show a plot of the generated distribution
-        mainPanel()
+        # Show a plot and table
+        mainPanel(
+          plotOutput('shapes'),
+          tableOutput('duration_table')
+        )
     )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {}
+server <- function(input, output) {
+  output$shapes <- renderPlot({}) #number of sightings by shape
+  output$duration_table <- renderTable({}) #number of sightings by shape, min/max/median/mean duration
+}
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
